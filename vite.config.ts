b47d8e70583929a8f99d4903d1da3daea06ab3f6
zakirthariq12 <zakirthariq52@gmail.com>
2025-12-15 00:58:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc";https://github.com/zakirthariq12/jerseytheory/edit/main/vite.config.ts
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({.base:'/',
+export default defineConfig(({ mode }) => ({.base:'/' ,
   server: {
     host: "::",
     port: 8080,
